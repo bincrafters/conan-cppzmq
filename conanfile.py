753e0d4bab1e426ba161a6971a314660cabca1cb
SSE4 <tomskside@gmail.com>
2018-02-01 13:00:48 +0700

    - initial prototype

--- conanfile.py
+++ conanfile.py
@@ -5,65 +5,44 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
-
-    # Indicates License type of the packaged library
+class CppZmqConan(ConanFile):
+    name = "cppzmq"
+    version = "4.2.2"
+    url = "https://github.com/bincrafters/conan-cppzmq"
+    description = "C++ binding for 0MQ"
     license = "MIT"
-
-    # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
-
-    # Remove following lines if the target lib does not use cmake.
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
+    # FIXME : we actually don't need these, but CMake helper require em
+    settings = ['compiler', 'arch']
 
-    # Options may need to change depending on the packaged library.
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-
-    # Custom attributes for Bincrafters recipe conventions
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
 
-    # Use version ranges for dependencies unless there's a reason not to
-    requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/[>=1.2.11]@conan/stable"
-    )
+    def requirements(self):
+        self.requires.add('zmq/[>=4.2.2]@bincrafters/stable')
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
+        source_url = "https://github.com/zeromq/cppzmq"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
 
-        #Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
 
-
     def build(self):
+        # TODO :
+        # ConanException: You must specify compiler, compiler.version and arch in your settings to use a CMake generator
+        tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
+                              'CMAKE_SOURCE_DIR',
+                              'CMAKE_CURRENT_SOURCE_DIR')
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
         cmake.configure(build_folder=self.build_subfolder)
         cmake.build()
         cmake.install()
 
     def package(self):
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can replace all the steps below with the word "pass"
-        include_folder = os.path.join(self.source_subfolder, "include")
         self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
 
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+    def package_id(self):
+        self.info.header_only()
