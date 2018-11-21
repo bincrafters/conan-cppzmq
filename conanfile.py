7eec91810a96eeefbfbfbe17c679299dc5b8b5e1
Uilian Ries <uilianries@gmail.com>
2018-11-21 17:36:22 -0200

    Bump minor version
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, CMake, tools
 
 class CppZmqConan(ConanFile):
     name = "cppzmq"
-    version = "4.2.2"
+    version = "4.3.0"
     url = "https://github.com/bincrafters/conan-cppzmq"
     homepage = "https://github.com/zeromq/cppzmq"
     description = "C++ binding for 0MQ"
@@ -21,17 +21,17 @@ class CppZmqConan(ConanFile):
     _source_subfolder = "source_subfolder"
 
     def requirements(self):
-        self.requires.add('zmq/4.2.2@bincrafters/stable')
+        self.requires.add('zmq/4.2.5@bincrafters/stable')
 
     def source(self):
         tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "CMAKE_SOURCE_DIR", "CMAKE_CURRENT_SOURCE_DIR")
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = CMake(self)
+        cmake.definitions["CPPZMQ_BUILD_TESTS"] = False
         cmake.configure()
         cmake.install()
 
