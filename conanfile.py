353fb7c7be2369b38ad084f99d7f3f7fe00c6910
Uilian Ries <uilianries@gmail.com>
2018-11-21 17:12:13 -0200

    Apply Conan Conventions
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,45 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
 import os
+from conans import ConanFile, CMake, tools
 
 
 class CppZmqConan(ConanFile):
     name = "cppzmq"
     version = "4.2.2"
     url = "https://github.com/bincrafters/conan-cppzmq"
+    homepage = "https://github.com/zeromq/cppzmq"
     description = "C++ binding for 0MQ"
+    author = "Bincrafters <bincrafters@gmail.com>"
     license = "MIT"
+    topics = ("conan", "cppzmq", "zmq-cpp", "zmq", "cpp-bind")
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-    settings = ['compiler', 'arch']
-
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
+    no_copy_source = True
+    _source_subfolder = "source_subfolder"
 
     def requirements(self):
         self.requires.add('zmq/4.2.2@bincrafters/stable')
 
     def source(self):
-        source_url = "https://github.com/zeromq/cppzmq"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = self.name + "-" + self.version
-
-        os.rename(extracted_dir, self.source_subfolder)
-
-    def build(self):
-        tools.replace_in_file(os.path.join(self.source_subfolder, 'CMakeLists.txt'),
-                              'CMAKE_SOURCE_DIR',
-                              'CMAKE_CURRENT_SOURCE_DIR')
-        cmake = CMake(self)
-        cmake.configure(build_folder=self.build_subfolder)
-        cmake.build()
-        cmake.install()
+        os.rename(extracted_dir, self._source_subfolder)
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "CMAKE_SOURCE_DIR", "CMAKE_CURRENT_SOURCE_DIR")
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.install()
 
     def package_id(self):
         self.info.header_only()
